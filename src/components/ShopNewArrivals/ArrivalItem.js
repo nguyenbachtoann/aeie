c1b6c45e222fd246068bfd87a9bec3a40fb361a9
Nguyễn Bách Toàn <nguyenbachtoan@Nguyens-MacBook-Pro-3.local>
2022-08-30 10:16:48 +0700

    test ssh

--- src/components/ShopNewArrivals/ArrivalItem.js
+++ src/components/ShopNewArrivals/ArrivalItem.js
@@ -1,26 +1,26 @@
-import { Col, Row } from "antd";
-import React from "react";
+import { Col, Row } from 'antd';
+import React from 'react';
 
 function ArrivalItem(item) {
   const renderItemInfo = () =>
     item.prices.map((x, i) => (
       <div key={i} className={`item-info-detail-${x.type}`}>
-        <Row justify="center" align="center">
+        <Row justify='center' align='center'>
           <Col span={24}>
-            <Row justify="center" align="center"  className="detail-row-container">
+            <Row justify='center' align='center' className='detail-row-container'>
               <Col span={10}>
-                <div className="divider-container">
-                  <div className="divider" />
+                <div className='divider-container'>
+                  <div className='divider' />
                 </div>
               </Col>
               <Col span={14}>
-                <div className="detail-container">
-                  <div className="detail-name">
-                    <span className="detail-name-text">{x.name}</span>
-                    <span className="detail-type-text">{x.type}</span>
+                <div className='detail-container'>
+                  <div className='detail-name'>
+                    <span className='detail-name-text'>{x.name}</span>
+                    <span className='detail-type-text'>{x.type}</span>
                   </div>
-                  <div className="detail-price">
-                    <span className="detail-price-text">${x.price}</span>
+                  <div className='detail-price'>
+                    <span className='detail-price-text'>${x.price}</span>
                   </div>
                 </div>
               </Col>
@@ -30,14 +30,10 @@ function ArrivalItem(item) {
       </div>
     ));
   return (
-    <div className="shop-arrival-item">
-      <div className="shop-item-container">
-        <img
-          src={item.image}
-          alt="shop-arrival-item"
-          className="shop-item-img"
-        />
-        <div className="item-info">{renderItemInfo()}</div>
+    <div className='shop-arrival-item'>
+      <div className='shop-item-container'>
+        <img src={item.image} alt='shop-arrival-item' className='shop-item-img' />
+        <div className='item-info'>{renderItemInfo()}</div>
       </div>
     </div>
   );
